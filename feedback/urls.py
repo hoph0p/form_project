c053d33cddb562c0e63a37042f486f5db9b312fa
Hop_Hop <novikov.hop.work@yandex.ru>
2022-07-11 17:36:31 +0300

    end?

---
+++ feedback/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('done', views.done),
+    path('', views.index),
+]
--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import FeedbackForm
+from .models import Feedback
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feed = Feedback(
+                name=form.cleaned_data['name'],
+                surname=form.cleaned_data['surname'],
+                feedback=form.cleaned_data['feedback'],
+                rating=form.cleaned_data['rating'],
+            )
+            feed.save()
+            return HttpResponseRedirect('/done')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback/feedback.html', context={'form': form})
+
+
+def done(request):
+    return render(request, 'feedback/done.html')
